ff5c2ce4758f8f75157bf78d2437b166969f8284
Nunzio Onorati <nunzioonorati@gmail.com>
2024-02-26 18:59:56 +0100

    Removed from plugins telescope and treesitter

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -47,24 +47,6 @@ return {
 
 
     {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-    },
-
-
-    { 
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' } 
-    },
-
-
-    {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-    },
-
-
-    {
         'nvim-lualine/lualine.nvim',
         dependencies = {
             "nvim-tree/nvim-web-devicons"
